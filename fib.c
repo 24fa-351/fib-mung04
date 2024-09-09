8c5ceda164b4b2a8bb76e1f978008aa5698fb059
Madison Ung <madison.ung@csu.fullerton.edu>
2024-09-08 22:29:10 -0700

    Minor adjustments to names and logical statements

--- fib.c
+++ fib.c
@@ -26,24 +26,23 @@ int fib_recursive(int n) {
 }
 
 int main(int argc, char* argv[]) {
-	FILE* fptr;
+	FILE* file_ptr;
 
-   fptr = fopen(argv[3], "r");
+   file_ptr = fopen(argv[3], "r");
 
-   if (fptr == NULL) {
-      printf("File not opened.");
+   if (file_ptr == NULL) {
       exit(0);
    }
    else {
       int file_num;
-      fscanf(fptr, "%d", &file_num);
+      fscanf(file_ptr, "%d", &file_num);
 
       int nth = file_num + atoi(argv[1]);
 
-      if (strcmp(argv[2], "-i")) {
+      if (strcmp(argv[2], "-i") || strcmp(argv[2], "i")) {
          printf("%d", fib_iterative(nth));
       }
-      else if (strcmp(argv[2], "-r")) {
+      else if (strcmp(argv[2], "-r") || strcmp(argv[2], "r")) {
          printf("%d", fib_recursive(nth - 1));
       }
       else {
